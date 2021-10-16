2f3478ea1606aedb4fb00069db0149dea65f520c
dankloop <hidasior@gmail.com>
2021-10-16 11:45:59 -0400

    API keys hidden

--- pics/src/api/unsplash.js
+++ pics/src/api/unsplash.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+import config from "../config";
 
 export default axios.create({
     baseURL: "https://api.unsplash.com",
     headers: {
-        Authorization: "Client-ID yKt8WDTuSalbc8vA3e1m8_WOY0-fO14iUaomKqd2rJ8",
+        Authorization: "Client-ID " + config.API_KEY,
     },
 });
--- videos/src/api/youtube.js
+++ videos/src/api/youtube.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+import config from "../config";
 
 export default axios.create({
     baseURL: "https://www.googleapis.com/youtube/v3",
     params: {
-        key: "AIzaSyCSVJFxBNRMncwd1iM5tzzpKVYiChEtMxo",
+        key: config.API_KEY,
     },
 });
