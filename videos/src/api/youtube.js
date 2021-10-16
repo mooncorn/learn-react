b42a46518d67f6bdfacb274a335d353996323e39
mooncorn <52269155+mooncorn@users.noreply.github.com>
2021-10-16 11:39:16 -0400

    Delete api directory

--- videos/src/api/youtube.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: "https://www.googleapis.com/youtube/v3",
-    params: {
-        key: "AIzaSyCSVJFxBNRMncwd1iM5tzzpKVYiChEtMxo",
-    },
-});
