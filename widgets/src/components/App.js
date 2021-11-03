fb2f1332d5fc1e8f52dbcf4a45e6dd8af9683f43
mooncorn <52269155+mooncorn@users.noreply.github.com>
2021-11-03 17:36:58 -0400

    translate component

--- widgets/src/components/App.js
+++ widgets/src/components/App.js
@@ -1,48 +1,13 @@
 import React, { useState } from "react";
 // import Accordion from "./Accordion";
 // import Search from "./Search";
-import Dropdown from "./Dropdown";
-
-// const items = [
-//   {
-//     title: "What is React?",
-//     content: "React is a front end javascript framework",
-//   },
-//   {
-//     title: "What use React?",
-//     content: "React is a favorite JS library among engineers",
-//   },
-//   {
-//     title: "How do you use React?",
-//     content: "You use React by creating components",
-//   },
-// ];
-
-const options = [
-  {
-    label: "The Color Red",
-    value: "red",
-  },
-  {
-    label: "The Color Green",
-    value: "green",
-  },
-  {
-    label: "The Shade Blue",
-    value: "blue",
-  },
-];
+// import Dropdown from "./Dropdown";
+import Translate from "./Translate";
 
 const App = () => {
-  const [selected, setSelected] = useState(options[0]);
-
   return (
     <div className="ui container">
-      <Dropdown
-        options={options}
-        selected={selected}
-        onSelectedChange={setSelected}
-      />
+      <Translate />
     </div>
   );
 };
