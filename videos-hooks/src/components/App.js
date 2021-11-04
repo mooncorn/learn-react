a0262ce7e161fc5981f93c23351e33191ccb8691
dankloop <hidasior@gmail.com>
2021-11-04 16:33:25 -0400

    custom hook

--- videos-hooks/src/components/App.js
+++ videos-hooks/src/components/App.js
@@ -2,47 +2,29 @@ import React, { useState, useEffect } from "react";
 import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
-import youtube from "../api/youtube";
+import useVideos from "../hooks/useVideos";
 // import Spinner from "./Spinner";
 
 const App = () => {
-  const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
 
+  const [videos, search] = useVideos("koala");
+
   useEffect(() => {
-    onTermSubmit("koala");
-  }, []);
-
-  const onTermSubmit = async (term) => {
-    try {
-      const results = await youtube.get("/search", {
-        params: {
-          q: term,
-        },
-      });
-
-      setVideos(results.data.items);
-      setSelectedVideo(results.data.items[0]);
-    } catch (err) {
-      console.log(err.message);
-    }
-  };
-
-  const onVideoSelect = (video) => {
-    setSelectedVideo(video);
-  };
+    setSelectedVideo(videos[0]);
+  }, [videos]);
 
   return (
     <div className="ui container">
       <h1>Videos</h1>
-      <SearchBar onTermSubmit={onTermSubmit} />
+      <SearchBar onTermSubmit={search} />
       <div className="ui grid">
         <div className="ui row">
           <div className="eleven wide column">
             <VideoDetail video={selectedVideo} />
           </div>
           <div className="five wide column">
-            <VideoList videos={videos} onVideoSelect={onVideoSelect} />
+            <VideoList videos={videos} onVideoSelect={setSelectedVideo} />
           </div>
         </div>
       </div>
---
+++ videos-hooks/src/hooks/useVideos.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import youtube from "../api/youtube";
+
+const useVideos = (defaultSearchTerm) => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    search(defaultSearchTerm);
+  }, []);
+
+  const search = async (term) => {
+    try {
+      const results = await youtube.get("/search", {
+        params: {
+          q: term,
+        },
+      });
+
+      setVideos(results.data.items);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  return [videos, search]; // react convention
+};
+
+export default useVideos;
