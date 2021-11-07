b9ada3223b729fe0195e8f66d33e6d12cde3ff37
dankloop <hidasior@gmail.com>
2021-11-07 15:50:17 -0500

    redux thunk

---
+++ blog/src/actions/index.js
@@ -0,0 +1,44 @@
+import _ from "lodash";
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonPlaceholder.get("/posts");
+
+  dispatch({
+    type: "FETCH_POSTS",
+    payload: response.data,
+  });
+};
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonPlaceholder.get("/users/" + id);
+
+  dispatch({
+    type: "FETCH_USER",
+    payload: response.data,
+  });
+};
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  // const userIds = _.uniq(_.map(getState().posts, "userId"));
+  // userIds.forEach((userId) => dispatch(fetchUser(userId)));
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
+};
+
+// MEMOIZE FETCH USER
+// export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get("/users/" + id);
+
+//   dispatch({
+//     type: "FETCH_USER",
+//     payload: response.data,
+//   });
+// });
---
+++ blog/src/reducers/postsReducer.js
@@ -0,0 +1,9 @@
+const postsReducer = (posts = [], action) => {
+  if (action.type === "FETCH_POSTS") {
+    return action.payload;
+  }
+
+  return posts;
+};
+
+export default postsReducer;
