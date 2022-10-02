49843f99e3a02d37a270414fc2d015ed221a2ffd
mooncorn <hidasior@gmail.com>
2022-10-02 19:29:02 -0400

    Update faker

--- components/src/index.js
+++ components/src/index.js
@@ -1,43 +1,45 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import CommentDetail from "./CommentDetail";
-import ApprovalCard from "./ApprovalCard";
-import faker from "faker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+import { faker } from '@faker-js/faker';
 
 const App = () => {
-    return (
-        <div className="ui container comments">
-            <ApprovalCard greenBtnText="Yes" redBtnText="No">
-                <h4>Warning!</h4>
-                Are you sure you want to do this?
-            </ApprovalCard>
+  return (
+    <div className="ui container comments" style={{ marginTop: '20px' }}>
+      <ApprovalCard greenBtnText="Yes" redBtnText="No">
+        <h4>Warning!</h4>
+        Are you sure you want to do this?
+      </ApprovalCard>
 
-            <ApprovalCard>
-                <CommentDetail
-                    author="Sam"
-                    timeAgo="Today at 5:21PM"
-                    commentText="Awsome!"
-                    srcAvatar={faker.image.avatar()}
-                />
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail
-                    author="Larry"
-                    timeAgo="Today at 5:22PM"
-                    commentText="Amazing!"
-                    srcAvatar={faker.image.avatar()}
-                />
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail
-                    author="Bob"
-                    timeAgo="Today at 5:23PM"
-                    commentText="Outstanding!"
-                    srcAvatar={faker.image.avatar()}
-                />
-            </ApprovalCard>
-        </div>
-    );
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.fullName()}
+          timeAgo="Today at 5:21PM"
+          commentText="Awsome!"
+          srcAvatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.fullName()}
+          timeAgo="Today at 5:22PM"
+          commentText="Amazing!"
+          srcAvatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.fullName()}
+          timeAgo="Today at 5:23PM"
+          commentText="Outstanding!"
+          srcAvatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+    </div>
+  );
 };
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector('#root'));
+
+console.log(faker.image.avatar());
